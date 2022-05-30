16fa2b2c4ad73b4fa50206c9947d9b2998821ad2
Aluno 8 <juansouzamd@gmail.com>
2022-05-30 18:08:30 -0300

    aula 30-05

---
+++ atividade 30-05/Jquery/javascript/script.js
@@ -0,0 +1,24 @@
+// $("#p1").text("Teste de texto");
+
+// $("#alvo" ).click(function () {
+//     alert("Usuário clicou no texto");
+// });
+
+// $("#txt1").mousedown(function(){
+//     $("#txt2").text("Botão do mouse para BAIXO");
+//     })
+//     .mouseup(function(){
+//      $("#txt2").text("Botão do mouse para CIMA");    
+//     });
+
+// $("#txt").keypress(function(){
+//     $("p").text($("#txt").val());        
+//         });
+
+$("#txt").keyup(function(){
+    $("#p1").text($("#txt").val());
+    $("#p2").text("");    
+    })
+    .keydown(function(){
+    $("#p2").text("tecla pressionada");
+    });
